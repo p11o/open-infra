9d6642f6d36e876e9a134ca76d98ac4337e38996
Matthew Prestifilippo <m.prestifilippo@gmail.com>
2022-04-01 11:58:02 -0400

    Template out dns

--- terraform/coredns.tf
+++ terraform/coredns.tf
@@ -5,6 +5,11 @@ resource "kubernetes_namespace" "coredns" {
   }
 }
 
+variable "nginx_tcp_ip_address" {
+  type        = string
+  description = "IP the tcp load balancer"
+}
+
 resource "helm_release" "coredns" {
   name      = "coredns"
   namespace = kubernetes_namespace.coredns.metadata.0.name
@@ -16,4 +21,9 @@ resource "helm_release" "coredns" {
   values = [
     file("./helm/coredns/values.yaml")
   ]
+
+  set {
+    name  = "servers[0].plugins[0].configBlock"
+    value = " answer \"{{ .Name }} 60 IN A ${var.nginx_tcp_ip_address}\" "
+  }
 }
