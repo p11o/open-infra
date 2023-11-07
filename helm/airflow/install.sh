717d2a036fb7e625090bba404d54c863fb2257a7
Matthew Prestifilippo <m.prestifilippo@gmail.com>
2023-11-07 17:41:35 -0500

    Remove helm

--- helm/airflow/install.sh
+++
@@ -1,5 +0,0 @@
-#/usr/bin/env bash
-
-
-helm repo add apache-airflow https://airflow.apache.org
-helm upgrade --install airflow apache-airflow/airflow --namespace airflow --create-namespace
--- helm/gitea/install.sh
+++
@@ -1,5 +0,0 @@
-#/usr/bin/env bash
-helm repo add gitea-charts https://dl.gitea.com/charts/
-helm repo update
-helm install -n gitea gitea gitea-charts/gitea --create-namespace -f ./helm/gitea/values.yaml
-helm upgrade --install -n gitea gitea gitea-charts/gitea --create-namespace -f ./helm/gitea/values.yaml
