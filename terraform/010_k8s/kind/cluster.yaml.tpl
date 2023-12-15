0c030b145c34b5b3d74fa3641bd3b3123be3cff3
Matthew Prestifilippo <m.prestifilippo@gmail.com>
2023-12-15 08:42:40 -0500

    Add cluster host volume for s3

--- terraform/010_k8s/kind/cluster.yaml.tpl
+++ terraform/010_k8s/kind/cluster.yaml.tpl
@@ -2,6 +2,9 @@ kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
 nodes:
 - role: control-plane
+  extraMounts:
+  - hostPath: ${host_s3_path}
+    containerPath: /var/s3
   kubeadmConfigPatches:
   - |
     kind: InitConfiguration
--- terraform/010_k8s/main.tf
+++ terraform/010_k8s/main.tf
@@ -1,13 +1,22 @@
 ############
 # Kubernetes
 
+variable "host_s3_path" {
+  default = "/tmp/s3"
+  type    = string
+}
+
+locals {
+  cluster_config = templatefile("${path.module}/kind/cluster.yaml.tpl", { host_s3_path = var.host_s3_path })
+}
+
 data "external" "check_kind_cluster" {
   program = ["bash", "${path.module}/scripts/check_cluster.sh"]
 }
 
 resource "null_resource" "kind_cluster" {
   provisioner "local-exec" {
-    command = data.external.check_kind_cluster.result.exists == "false" ? "kind create cluster --config ${path.module}/kind/cluster.yaml" : "echo 'Cluster already exists'"
+    command = data.external.check_kind_cluster.result.exists == "false" ? "kind create cluster --config - <<EOF\n${local.cluster_config}\nEOF" : "echo 'Cluster already exists'"
   }
   provisioner "local-exec" {
     when    = destroy
